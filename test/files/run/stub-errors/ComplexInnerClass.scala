08f5197cef40498e90c7d7bdcb3076e3df54d19f
jvican <jorgevc@fastmail.es>
2016-12-29 11:01:38 +0100

    Check init of symbol doesn't modify stub error message

--- test/files/run/stub-errors/ComplexInnerClass.scala
+++ test/files/run/stub-errors/ComplexInnerClass.scala
@@ -21,11 +21,20 @@ object Test extends scala.tools.partest.StubErrorMessageTest {
     class C {
       def aloha = {
         val b = new B
+        val d = new extra.D
+        d.foo
         println(b.foo)
         new b.BB
       }
     }
   """
+
+  override def extraUserCode = """
+    package extra
+    class D {
+      def foo = "Hello, World"
+    }
+  """.stripMargin
 
   def removeFromClasspath(): Unit = {
     removeClasses("stuberrors", List("A"))
